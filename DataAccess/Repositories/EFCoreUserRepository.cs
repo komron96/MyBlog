a7ec9ceec59fa3456975685b7f05660196171eb4
komron96 <komron.saydaliev@gmail.com>
2023-12-01 11:00:34 +0500

    forget to add await in Posts.AddAsync()

--- DataAccess/Repositories/EFCoreUserRepository.cs
+++ DataAccess/Repositories/EFCoreUserRepository.cs
@@ -37,7 +37,7 @@ public sealed class EFCoreUserRepository : IUserRepository
 
     public async Task<bool> DeleteUserAsync(long userId, CancellationToken token = default)
     {
-        User user = await _appDbContext.Users.FirstOrDefaultAsync(p => p.Id == userId);
+        User user = await _appDbContext.Users.FirstOrDefaultAsync(p => p.Id == userId, cancellationToken: token);
         if (user != null)
         {
             _appDbContext.Users.Remove(user);
